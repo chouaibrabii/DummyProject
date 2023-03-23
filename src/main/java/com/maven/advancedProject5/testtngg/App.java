3bb078f441a3f21e4d16859ad9d4872394e11b2c
chouaibrabii <chouaibrabii@gmail.com>
2023-03-22 21:04:18 -0400

    first commit

---
+++ src/main/java/com/maven/advancedProject5/testtngg/App.java
@@ -0,0 +1,13 @@
+package com.maven.advancedProject5.testtngg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/maven/advancedProject5/testtngg/LoginTest.java
@@ -0,0 +1,11 @@
+package com.maven.advancedProject5.testtngg;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void loginUser() {
+		System.out.println("This is login user test");
+	}
+}
